79f454b2496d1b5e12a086b8c1472385bfd7c70b
zeffah <zeffah.elly@gmail.com>
2023-07-21 00:30:45 +0300

    -removed .idea from vcs

--- app/src/main/java/com/julietolieng/hackathon1/ui/LoginFragment.kt
+++ app/src/main/java/com/julietolieng/hackathon1/ui/LoginFragment.kt
@@ -8,18 +8,8 @@ import androidx.fragment.app.Fragment
 import com.julietolieng.hackathon1.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-    }
-
-    companion object {
-        @JvmStatic
-        fun newInstance() = LoginFragment().apply {
-            arguments = Bundle().apply {}
-        }
     }
 }
